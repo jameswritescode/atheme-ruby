da0a3a78f7e62fa5d36bd3e7a36104e8efe3e13c
James Newton <james@Zaphyous.com>
2013-03-20 17:52:32 -0500

    rspec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,20 @@
 require 'rspec'
+require 'pry'
+require 'atheme'
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |file| require file }
+
+def atheme_config
+  YAML.load_file(File.expand_path('../config.yml', __FILE__))
+end
+
+def configure_atheme
+  Atheme.configure do |config|
+    config.port = atheme_config['port']
+    config.url = atheme_config['url']
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:each) { configure_atheme }
+end
