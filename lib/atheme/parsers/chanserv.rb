6511e8cb86038067182d763f935be93a7b716d6a
James Newton <james@Zaphyous.com>
2013-07-18 20:41:41 -0400

    Add ChanServ.list.to_a - which returns an array of hashes with channel and owner

--- lib/atheme/parsers/chanserv.rb
+++ lib/atheme/parsers/chanserv.rb
@@ -12,4 +12,20 @@ module Atheme::Parser::ChanServ
       raw_services_output.match(/Successor\s+:\s+\(none\)/) ? nil : match(/Successor\s+:\s+(\w+)/)
     end
   end
+
+  parse :list do
+    command :to_a do
+      output = raw_services_output.split("\n")
+      output.delete_at(0)
+      output.delete_at(output.length - 1)
+
+      output.map do |info|
+        out     = info.sub('- ', '').split('(')
+        channel = out[0].sub(' ', '')
+        owner   = out[1].sub(')', '').sub(' ', '')
+
+        { channel: channel, owner: owner }
+      end
+    end
+  end
 end
